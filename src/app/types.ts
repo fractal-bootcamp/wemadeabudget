0317a644d1e9f9cc725a6a44d89bcc8a060615ba
Brian Smiley <briantsmiley42@gmail.com>
2024-08-07 10:52:52 -0400

    adds local types stuyff

---
+++ src/app/types.ts
@@ -0,0 +1,27 @@
+export interface User {
+  username: string;
+  email: string;
+  clerkId: string;
+}
+export type AccountType = "CHECKING" | "CASH" | "CREDIT_CARD";
+export type Flag = "RED" | "ORANGE" | "YELLOW" | "GREEN" | "BLUE" | "PURPLE";
+export type TransactionDetails = {
+  id: string;
+  account: string;
+  category: string;
+  payee: string;
+  date: string;
+  cents: number;
+  memo: string;
+  flag: Flag;
+  reconciled: boolean;
+};
+
+export interface AccountDetails {
+  name: string;
+  type: AccountType;
+}
+
+export interface Category {
+  name: string;
+}
