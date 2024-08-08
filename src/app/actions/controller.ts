e9fadb34e0383fcda75b49424257d5f0746ab8c9
Brian Smiley <briantsmiley42@gmail.com>
2024-08-08 15:15:20 -0400

    adds a client facing controller layer to backend db function services

---
+++ src/app/actions/controller.ts
@@ -0,0 +1,25 @@
+import clerkHandler from "../middleware/clerkHandler";
+import { transactionServices } from "../services/transactions";
+
+const attachUserId =
+  <T extends any[], R>(callback: (userId: string, ...args: T) => Promise<R>) =>
+  async (...details: T): Promise<R> => {
+    const { authenticated, user } = await clerkHandler();
+    if (!authenticated || user === null) {
+      throw new Error("User is not authenticated");
+    }
+    return await callback(user.id, ...details);
+  };
+
+export const clientController = {
+  transaction: {
+    add: attachUserId(transactionServices.add),
+    delete: attachUserId(transactionServices.delete),
+    getById: attachUserId(transactionServices.getById),
+    getAllByUser: attachUserId(transactionServices.getAllByUser),
+    update: attachUserId(transactionServices.update)
+  },
+  category: {},
+  payee: {},
+  account: {}
+};
