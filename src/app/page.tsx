274fd311967251ab2059777bc950f1c3c9d0f05a
Brian Smiley <briantsmiley42@gmail.com>
2024-08-09 09:05:48 -0400

    adds loading page and account database fetch on initial page load

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,12 @@
 'use server'
 import { transactionGetAllByUser } from './actions/controller'
 import Sandbox from './components/Sandbox'
-import { useEffect } from 'react'
-import useTransactionStore from './stores/transactionStore'
-const MIN_LOADING_TIME = 2000
+const MIN_LOADING_TIME = 3000
 export default async function Home() {
   const time = new Date().getTime()
   const transactions = await transactionGetAllByUser()
   const elapsed = new Date().getTime() - time
-  const remainingTime = MIN_LOADING_TIME - elapsed
+  const remainingTime = Math.max(MIN_LOADING_TIME - elapsed, 0)
   await new Promise((resolve) => setTimeout(resolve, remainingTime))
 
   return <Sandbox transactions={transactions} />
