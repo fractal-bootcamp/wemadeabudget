329136ace3fb0059d5d6e3096d9f15d493026582
Brian Smiley <briantsmiley42@gmail.com>
2024-08-10 23:52:31 -0400

    tweaks button color/hover on splash

--- src/app/sign-up/page.tsx
+++ src/app/sign-up/page.tsx
@@ -6,13 +6,13 @@ const figtree = Figtree({ subsets: ['latin'], weight: '600' })
 export default function SignUpOrIn() {
   return (
     <div
-      className={`${figtree.className} flex h-screen w-screen flex-col items-center justify-center gap-5 bg-slate-100 text-indigo-600`}
+      className={`${figtree.className} flex h-screen w-screen flex-col items-center justify-center gap-5 bg-slate-100 text-indigo-700`}
     >
       <span className="animate-[fadeIn_2s_forwards] text-5xl opacity-0">
         🌱 We Need A Budget
       </span>
       <SignInButton mode="modal">
-        <span className="animate-[fadeIn_1s_ease-in-out_1s_forwards] cursor-pointer rounded bg-indigo-200 px-4 py-2 text-2xl opacity-0">
+        <span className="animate-[fadeIn_1s_ease-in-out_1s_forwards] cursor-pointer rounded-lg bg-indigo-200 px-4 py-2 text-2xl opacity-0 transition-all duration-300 hover:bg-indigo-300">
           Sign in/up
         </span>
       </SignInButton>
