18647b04c35ba7a9762fa17269f3a1d4e47e593c
Brian Smiley <briantsmiley42@gmail.com>
2024-08-10 22:56:10 -0400

    tweaks timings

--- src/app/components/ReflectPage/Reflect.tsx
+++ src/app/components/ReflectPage/Reflect.tsx
@@ -6,13 +6,13 @@ export default function Reflect() {
     <div className="flex w-full items-center justify-center bg-white">
       <div className="flex flex-col items-center justify-center gap-4">
         <span
-          className={`${figtree.className} animate-[fadeIn_3s_ease-in-out_6s_forwards] opacity-0`}
+          className={`${figtree.className} animate-[fadeIn_3s_ease-in-out_5s_forwards] opacity-0`}
         >
           Reflect on your finances...
         </span>
         <div className="flex items-end justify-center">
           <Image
-            className="animate-[fadeIn_2.5s_ease-in-out_3s_forwards] opacity-0"
+            className="animate-[fadeIn_2s_ease-in-out_2s_forwards] opacity-0"
             src="/lotus.svg"
             alt="lotus"
             width={100}
@@ -20,14 +20,14 @@ export default function Reflect() {
           />
           <div className="relative flex flex-col items-center justify-center">
             <div className="absolute left-1/2 top-2 -translate-x-1/2">
-              <span className="relative flex h-10 w-10 animate-[fadeIn_0s_ease-in-out_9s_forwards] items-center justify-center opacity-0">
+              <span className="relative flex h-10 w-10 animate-[fadeIn_0s_ease-in-out_7s_forwards] items-center justify-center opacity-0">
                 <span className="animate-ping-slow absolute inline-flex h-full w-full rounded-full bg-black opacity-75 transition delay-[9000ms]"></span>
                 <span className="relative inline-flex h-1 w-1 rounded-full bg-black"></span>
               </span>
             </div>
 
             <Image
-              className="animate-[fadeIn_2s_ease-in-out_.5s_forwards] opacity-0"
+              className="animate-[fadeIn_2s_ease-in-out_.1s_forwards] opacity-0"
               src="/reflect.svg"
               alt="reflect"
               width={200}
@@ -35,7 +35,7 @@ export default function Reflect() {
             />
           </div>
           <Image
-            className="animate-[fadeIn_2.5s_ease-in-out_3s_forwards] opacity-0"
+            className="animate-[fadeIn_2s_ease-in-out_2s_forwards] opacity-0"
             src="/lotus.svg"
             alt="lotus"
             width={100}
