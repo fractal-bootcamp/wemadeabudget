46ae8cf72931dba46ed23e464ec8c1e24f70b2d8
Brian Smiley <briantsmiley42@gmail.com>
2024-08-10 23:44:51 -0400

    makes signin a modal

--- src/app/sign-up/page.tsx
+++ src/app/sign-up/page.tsx
@@ -11,7 +11,7 @@ export default function SignUpOrIn() {
       <span className="animate-[fadeIn_2s_forwards] text-5xl opacity-0">
         🌱 We Need A Budget
       </span>
-      <SignInButton>
+      <SignInButton mode="modal">
         <span className="animate-[fadeIn_1s_ease-in-out_1s_forwards] cursor-pointer rounded bg-indigo-200 px-4 py-2 text-2xl opacity-0">
           Sign in/up
         </span>
