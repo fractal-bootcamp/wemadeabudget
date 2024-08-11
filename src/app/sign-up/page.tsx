ca8cfe507e3bf7552d79397eafd3c07d4c2de8c1
Brian Smiley <briantsmiley42@gmail.com>
2024-08-10 23:43:33 -0400

    makes signup splash prettier

--- src/app/sign-up/page.tsx
+++ src/app/sign-up/page.tsx
@@ -1,5 +1,21 @@
-import {  SignUp } from "@clerk/nextjs";
+import { SignInButton, SignUp } from '@clerk/nextjs'
+
+import { Figtree } from 'next/font/google'
+const figtree = Figtree({ subsets: ['latin'], weight: '600' })
 
 export default function SignUpOrIn() {
-    return <SignUp />
+  return (
+    <div
+      className={`${figtree.className} flex h-screen w-screen flex-col items-center justify-center gap-5 text-indigo-600`}
+    >
+      <span className="animate-[fadeIn_2s_forwards] text-5xl opacity-0">
+        🌱 We Need A Budget
+      </span>
+      <SignInButton>
+        <span className="animate-[fadeIn_1s_ease-in-out_1s_forwards] cursor-pointer rounded bg-indigo-200 px-4 py-2 text-2xl opacity-0">
+          Sign in/up
+        </span>
+      </SignInButton>
+    </div>
+  )
 }
