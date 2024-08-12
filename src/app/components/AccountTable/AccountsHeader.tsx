39fc5a934452cb1e571c73b834cd93b38d12803b
sarahbicknell <166096006+sarahbicknell@users.noreply.github.com>
2024-08-12 02:50:47 -0400

    cleared displays dynamically

--- src/app/components/AccountTable/AccountsHeader.tsx
+++ src/app/components/AccountTable/AccountsHeader.tsx
@@ -1,19 +1,33 @@
 import { UserButton } from "@clerk/nextjs"
+import useBudgetStore from '../../stores/transactionStore' // Adjust the import path as needed
+
 interface AccountsHeaderProps {
   accountName: string | null
 }
+
 const AccountsHeader = ({ accountName }: AccountsHeaderProps) => {
+  const getBalanceByAccount = useBudgetStore(state => state.getBalanceByAccount)
+  const transactions = useBudgetStore(state => state.transactions)
+
+  const clearedBalance = accountName 
+    ? getBalanceByAccount(accountName) / 100 
+    : transactions.reduce((acc, t) => t.cleared ? acc + t.cents : acc, 0) / 100
+
+  const unclearedBalance = accountName
+    ? transactions.filter(t => t.account === accountName && !t.cleared).reduce((acc, t) => acc + t.cents, 0) / 100
+    : transactions.reduce((acc, t) => !t.cleared ? acc + t.cents : acc, 0) / 100
+
+  const workingBalance = clearedBalance + unclearedBalance
+
   return (
-    //TODO make these numbers dynamic
     <div className="flex flex-col">
       <div className="mx-4 my-2 text-xl font-semibold flex justify-between">
-        {' '}
         {accountName ?? 'All Accounts'}
         <UserButton />
       </div>
       <div className="flex w-full border-b border-t border-gray-300 p-2">
         <div className="flex flex-col px-2">
-          <div> -$1400.00 </div>
+          <div>{clearedBalance.toFixed(2)}</div>
           <div className="flex items-center gap-1 text-[10px]">
             <div className="flex h-3 w-3 items-center justify-center rounded-full bg-gray-800 font-bold text-white">
               C
@@ -23,7 +37,7 @@ const AccountsHeader = ({ accountName }: AccountsHeaderProps) => {
         </div>
         <div className="px-2"> + </div>
         <div className="flex flex-col px-2">
-          <div className="text-green-600"> $1400.00 </div>
+          <div className="text-green-600">{unclearedBalance.toFixed(2)}</div>
           <div className="flex items-center gap-1 text-[10px]">
             <div className="flex h-3 w-3 items-center justify-center rounded-full border border-gray-500 font-bold text-gray-500">
               C
@@ -33,11 +47,12 @@ const AccountsHeader = ({ accountName }: AccountsHeaderProps) => {
         </div>
         <div className="px-2"> = </div>
         <div className="flex flex-col px-2">
-          <div> $1400.00 </div>
+          <div>{workingBalance.toFixed(2)}</div>
           <div className="text-[10px]"> Working Balance </div>
         </div>
       </div>
     </div>
   )
 }
+
 export default AccountsHeader
