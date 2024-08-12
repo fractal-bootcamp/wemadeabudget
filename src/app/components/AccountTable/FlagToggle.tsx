22cca9e11322bec1fa91f3b62937f911618b7739
Brian Smiley <briantsmiley42@gmail.com>
2024-08-12 18:14:54 -0400

    refactors type detail export stuff

--- src/app/components/AccountTable/FlagToggle.tsx
+++ src/app/components/AccountTable/FlagToggle.tsx
@@ -1,20 +1,20 @@
 'use client'
 import { Bookmark } from 'lucide-react'
-import { Flag, flagColors } from '../../types'
-import { useMemo } from 'react'
+import { Flag, FlagDetails } from '../../types'
 
 interface FlogTaggleProps {
   flag: Flag
   onToggle: () => void
 }
 export default function FlogTaggle({ onToggle, flag }: FlogTaggleProps) {
+  const flagDetails = FlagDetails[flag]
   return (
     <div onClick={onToggle}>
       <Bookmark
         className="rotate-[270deg] transform text-gray-400"
         id="form-flag"
         size={16}
-        fill={flagColors[flag]}
+        fill={flagDetails.hexCode}
       />
       <input type="hidden" name="flag" value={flag} />
     </div>
