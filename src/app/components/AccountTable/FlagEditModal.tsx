4c66318e2972a83a0ce2bf29ed97b4a256020701
Brian Smiley <briantsmiley42@gmail.com>
2024-08-13 14:02:04 -0400

    flag modal on new and edit transactions?

---
+++ src/app/components/AccountTable/FlagEditModal.tsx
@@ -0,0 +1,61 @@
+import { Flag } from '@prisma/client'
+import { FlagDetails, typeDetailsArray } from '../../types'
+import { Bookmark, Check } from 'lucide-react'
+
+const lighterHexCodes: { [key: string]: string } = {
+  '#FF0000': '#FF6666', // Red
+  '#00FF00': '#66FF66', // Green
+  '#0000FF': '#6666FF', // Blue
+  '#FFFF00': '#FFFF66', // Yellow
+  '#00FFFF': '#66FFFF', // Cyan
+  '#FF00FF': '#FF66FF', // Magenta
+  '#000000': '#666666', // Black
+  '#FFFFFF': '#FFFFFF', // White (unchanged)
+  '#808080': '#B3B3B3', // Gray
+}
+
+interface FlagEditModalProps {
+  currentFlag: Flag
+  onFlagSelect: (flag: Flag) => void
+}
+export default function FlagEditModal({
+  currentFlag,
+  onFlagSelect,
+}: FlagEditModalProps) {
+  return (
+    <div
+      className="absolute left-1/2 top-full z-50 flex w-[125px] -translate-x-1/2 translate-y-4 flex-col items-center gap-2 rounded-md border bg-white p-3 shadow-2xl"
+      onClick={(e) => e.stopPropagation()}
+      style={{ overflow: 'visible' }}
+    >
+      <div className="absolute -top-2 left-1/2 h-4 w-4 -translate-x-1/2 rotate-45 transform border-l border-t border-gray-200 bg-white" />
+      {typeDetailsArray(FlagDetails).map((flagOption) => (
+        <button
+          onClick={() => onFlagSelect(flagOption.type)}
+          className="flex w-20 gap-1 rounded-full border border-slate-300 px-3 py-1 text-[10px] font-light text-gray-400"
+          key={flagOption.display}
+          style={{
+            backgroundColor:
+              currentFlag === flagOption.type
+                ? lighterHexCodes[flagOption.hexCode]
+                : '',
+          }}
+        >
+          {currentFlag === flagOption.type ? (
+            <Check className="h-4 w-4 text-slate-700" />
+          ) : (
+            <Bookmark
+              className="rotate-[270deg] transform text-gray-400"
+              size={16}
+              fill={flagOption.hexCode}
+              color={
+                flagOption.hexCode === '#ffffff' ? 'gray' : flagOption.hexCode
+              }
+            />
+          )}
+          {flagOption.display}
+        </button>
+      ))}
+    </div>
+  )
+}
--- src/app/components/AccountTable/FlagToggle.tsx
+++ src/app/components/AccountTable/FlagToggle.tsx
@@ -1,71 +1,52 @@
-import React, { useState } from 'react';
-import { Bookmark, Check } from 'lucide-react';
-import { Flag, FlagDetails, typeDetailsArray } from '../../types';
+import React, { useState } from 'react'
+import { Bookmark, Check } from 'lucide-react'
+import {
+  Flag,
+  FlagDetails,
+  TransactionDetails,
+  typeDetailsArray,
+} from '../../types'
+import FlagEditModal from './FlagEditModal'
+import { METHODS, updateStoreAndDb } from '../../util/utils'
+import { dbTransactionUpdate } from '../../actions/controller'
+import useBudgetStore from '../../stores/transactionStore'
 
 interface FlagToggleProps {
-  flag: Flag
+  currentFlag: Flag
+  onFlagSelect: (flag: Flag) => void
 }
 
-const lighterHexCodes: { [key: string]: string } = {
-  "#FF0000": "#FF6666", // Red
-  "#00FF00": "#66FF66", // Green
-  "#0000FF": "#6666FF", // Blue
-  "#FFFF00": "#FFFF66", // Yellow
-  "#00FFFF": "#66FFFF", // Cyan
-  "#FF00FF": "#FF66FF", // Magenta
-  "#000000": "#666666", // Black
-  "#FFFFFF": "#FFFFFF", // White (unchanged)
-  "#808080": "#B3B3B3"  // Gray
-};
+export default function FlagToggle({
+  currentFlag,
+  onFlagSelect,
+}: FlagToggleProps) {
+  const [showFlagModal, setShowFlagModal] = useState(false)
 
-function FlagModal({flagColor, setFlagColor}: {flagColor: string, setFlagColor: (flagColor: string) => void}) {
-  return (
-    <div
-      className="absolute top-full left-1/2 z-50 flex -translate-x-1/2 translate-y-4 flex-col items-center gap-2 w-[125px] rounded-md border bg-white p-3 shadow-2xl"
-      onClick={(e) => e.stopPropagation()}
-      style={{overflow: 'visible'}}
-    >
-      <div className="absolute -top-2 left-1/2 h-4 w-4 -translate-x-1/2 rotate-45 transform border-t border-l border-gray-200 bg-white" />
-      {typeDetailsArray(FlagDetails).map((flagOption) => (
-        <button 
-          onClick={() => setFlagColor(flagOption.hexCode)}
-          className="w-20 border rounded-full border-slate-300 px-3 py-1 flex gap-1 text-gray-400 font-light text-[10px]" 
-          key={flagOption.display}
-          style={{ backgroundColor: flagColor === flagOption.hexCode ? lighterHexCodes[flagOption.hexCode] : "" }}
-        >
-          {flagColor === flagOption.hexCode ? <Check className="h-4 w-4 text-slate-700" /> :
-          <Bookmark
-            className="rotate-[270deg] transform text-gray-400"
-            size={16}
-            fill={flagOption.hexCode}
-            color={flagOption.hexCode === "#ffffff" ? "gray" : flagOption.hexCode} /> }
-            {flagOption.display}
-        </button> 
-      ))}
-    </div>
-  )
-}
-
-export default function FlagToggle({ flag }: FlagToggleProps) {
-  const [flagColor, setFlagColor] = useState<string>(FlagDetails[flag].hexCode);
-  const [showFlagModal, setShowFlagModal] = useState(false);
-
-  const handleToggle = (e: React.MouseEvent) => {
-    e.stopPropagation();
-    setShowFlagModal(prev => !prev);
+  const handleToggleModal = (e: React.MouseEvent) => {
+    e.stopPropagation()
+    setShowFlagModal((prev) => !prev)
   }
 
   return (
-    <div className="relative" onClick={handleToggle}>
+    <div className="relative" onClick={handleToggleModal}>
       <Bookmark
         className="rotate-[270deg] transform text-gray-400"
         id="form-flag"
         size={16}
-        fill={flagColor === "#ffffff" ? "transparent" : flagColor}
-        color={flagColor === "#ffffff" ? "gray" : flagColor}
+        fill={
+          FlagDetails[currentFlag].hexCode === '#ffffff'
+            ? 'transparent'
+            : FlagDetails[currentFlag].hexCode
+        }
+        color={
+          FlagDetails[currentFlag].hexCode === '#ffffff'
+            ? 'gray'
+            : FlagDetails[currentFlag].hexCode
+        }
       />
-      {showFlagModal && <FlagModal flagColor={flagColor} setFlagColor={setFlagColor} />}
-      <input type="hidden" name="flag" value={flag} />
+      {showFlagModal && (
+        <FlagEditModal currentFlag={currentFlag} onFlagSelect={onFlagSelect} />
+      )}
     </div>
   )
 }
