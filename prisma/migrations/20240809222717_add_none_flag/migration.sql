ec20db29cb96da1c2df9bf16ddfb38318d304e6a
Brian Smiley <briantsmiley42@gmail.com>
2024-08-19 01:39:57 -0400

    resets db

--- prisma/migrations/20240809222717_add_none_flag/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterEnum
-ALTER TYPE "Flag" ADD VALUE 'NONE';
--- prisma/migrations/20240818152510_transfers/migration.sql
+++
@@ -1,5 +0,0 @@
--- DropForeignKey
-ALTER TABLE "Transaction" DROP CONSTRAINT "Transaction_pairedTransferId_fkey";
-
--- AddForeignKey
-ALTER TABLE "Transaction" ADD CONSTRAINT "Transaction_pairedTransferId_fkey" FOREIGN KEY ("pairedTransferId") REFERENCES "Transaction"("id") ON DELETE CASCADE ON UPDATE CASCADE;
