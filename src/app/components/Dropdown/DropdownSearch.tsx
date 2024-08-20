4cd5e21b256a4d182241dfda2b90e49b3fb438d2
Brian Smiley <briantsmiley42@gmail.com>
2024-08-20 12:32:54 -0400

    fixes some dropdown focus/blur stuff

--- src/app/components/Dropdown/DropdownSearch.tsx
+++ src/app/components/Dropdown/DropdownSearch.tsx
@@ -2,22 +2,26 @@ import { Search } from 'lucide-react'
 
 interface DropdownSearchProps {
   searchTerm: string
+  inputRef: React.RefObject<HTMLInputElement>
   setSearchTerm: (searchTerm: string) => void
   setExpanded: (expanded: boolean) => void
 }
 const DropdownSearch = ({
   searchTerm,
+  inputRef,
   setSearchTerm,
   setExpanded,
 }: DropdownSearchProps) => {
   return (
-    <div className={`relative w-full cursor-pointer bg-slate-50`}>
+    <div className={`relative w-full cursor-pointer rounded-lg bg-slate-50`}>
       <input
         type="text"
         className="w-full bg-slate-200 py-2 pl-4 pr-6"
         placeholder="Search or Add"
         value={searchTerm}
+        ref={inputRef}
         onFocus={() => setExpanded(true)}
+        onBlur={() => setExpanded(false)}
         onChange={(e) => setSearchTerm(e.target.value)}
       />
       <div className="absolute right-0 top-1/2 -translate-y-1/2 p-1 text-gray-400">
