2c8f026d6d27c2470997b7c2279ec270df72b942
Brian Smiley <briantsmiley42@gmail.com>
2025-01-09 23:53:18 -0500

    cleans up clerk middlware removing old unused protected route stuff

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,13 +1,7 @@
-import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
+import { clerkMiddleware } from '@clerk/nextjs/server'
 
-let reqRecord = {}
-
-const isProtectedRoute = createRouteMatcher(['/create(.*)'])
-// This Middleware does not protect any routes by default.
-// See https://clerk.com/docs/references/nextjs/clerk-middleware for more information about configuring your Middleware
-export default clerkMiddleware((auth, req) => {
-  if (isProtectedRoute(req)) auth().protect()
-})
+// This example protects all routes including api/trpc routes
+export default clerkMiddleware()
 
 export const config = {
   matcher: ['/((?!.*\\..*|_next).*)', '/', '/_next(.*)', '/(api|trpc)(.*)'],
